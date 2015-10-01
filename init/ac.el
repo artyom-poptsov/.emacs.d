9018837e63ec316d1a757ab42ead53b3a169cec0
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2015-10-01 21:50:03 +0300

    init/ac.el: Add to the repository

---
+++ init/ac.el
@@ -0,0 +1,10 @@
+;;; ac.el -- Emacs Auto Complete mode initialization.
+
+;; Required packages
+(require 'popup)
+(require 'auto-complete)
+
+;; Enable AC globally.
+(global-auto-complete-mode t)
+
+;;; ac.el ends here.
