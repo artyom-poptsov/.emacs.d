aaffc5c01a1f4eec2d6d71ab958835829d3421ba
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2015-10-08 05:11:46 +0300

    eshell.el: Add to the repository

---
+++ init/eshell.el
@@ -0,0 +1,30 @@
+;;; eshell.el -- Eshell configuration
+
+(require 'eshell)
+
+(setq eshell-directory-name (expand-file-name "eshell" *emacs-local-dir*))
+
+(setenv "PAGER" "cat")
+(setenv "GIT_PAGER" "cat")
+(setenv "TERM" "xterm-256color")
+
+(setq eshell-history-size 2048)
+(setq eshell-prompt-regexp "^.*[#$] ")
+
+(defun pwd-repl-home (pwd)
+  (interactive)
+  (let* ((home     (expand-file-name (getenv "HOME")))
+         (home-len (length home)))
+    (if (and (>= (length pwd) home-len)
+             (equal home (substring pwd 0 home-len)))
+        (concat "~" (substring pwd home-len))
+      (car (reverse (split-string pwd "[/]"))))))
+
+;; Configure the prompt
+(setq eshell-prompt-function
+  (lambda ()
+    (concat (user-login-name) "@" (system-name) " "
+            (format-time-string "%Y-%m-%d %H:%M" (current-time)) "\n"
+            (pwd-repl-home (eshell/pwd)) (if (= (user-uid) 0) " # " " $ "))))
+
+;;; eshell.el ends here
