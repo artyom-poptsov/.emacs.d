60d550a9d3394a9710f753b5d2ecd5d0a9d7b72e
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2016-03-07 00:54:41 +0300

    init/javascript.el: Update

--- init/javascript.el
+++ init/javascript.el
@@ -1,6 +1,9 @@
 ;;; javascript.el -- JavaScript mode configuration.
 
-(font-lock-add-keywords 'js-mode
+;; setup files ending in “.js” to open in js2-mode
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+
+(font-lock-add-keywords 'js2-mode
 			'(("^\\(\/\/\/\\)\\([ \t]*\\([^\r\n]*\\)\r?\n?\\)"
 			   (1 font-lock-firm-comment-semi-face prepend)
 			   (2 font-lock-firm-comment-text-face prepend))))
